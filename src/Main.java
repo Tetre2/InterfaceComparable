66aebdde9a0d1e62a6ae12469ca3835f3e302c48
Tetre2 <mattias.retteli@gmail.com>
2017-11-21 10:25:58 +0100

    skapat en main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Person p = new Person("retteli fsddfs sdadsa mattias");
+		
+		System.out.println(p.getfName());
+		System.out.println(p.getlName());		
+		
+	}
+	
 }
