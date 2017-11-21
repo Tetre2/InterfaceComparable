e91944844fd4d86c1138f87e82942451c07883be
Tetre2 <mattias.retteli@gmail.com>
2017-11-21 10:55:47 +0100

    läger till nya personer i en arrayList

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		Person p = new Person("retteli fsddfs sdadsa mattias");
+		ArrayList<Person> persons = new ArrayList<>();
 		
-		System.out.println(p.getfName());
-		System.out.println(p.getlName());		
+		Scanner sc = new Scanner(new File("Persons.txt"));
 		
+		
+		while(sc.hasNextLine()){
+			Person p = new Person(sc.nextLine());
+			
+			persons.add(p);
+		}
+		
+		
+		System.out.println(persons.size());
+				
+		sc.close();
 	}
 	
 }
