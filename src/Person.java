1df9aaa30fbd32ae0564918478588fcf593ee81d
Tetre2 <mattias.retteli@gmail.com>
2017-11-21 10:07:55 +0100

    la till getters och setters

--- src/Person.java
+++ src/Person.java
@@ -7,5 +7,25 @@ public class Person {
 	public Person(){
 		
 	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	
+	
+	
+	
 	
 }
